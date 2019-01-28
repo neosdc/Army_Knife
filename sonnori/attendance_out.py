0b842d5afa59c0fe3a36ce3618a7aa2328c71ef3
Kim Seung Young <neosdc@gmail.com>
2019-01-28 13:17:34 +0900

    손노리 - 출, 퇴근 스크립트

---
+++ sonnori/attendance_out.py
@@ -0,0 +1,40 @@
+import asyncio
+from pyppeteer import launch
+from time import sleep
+
+async def close_dialog(dialog):
+    print("dialog popup")
+    await dialog.dismiss()
+
+
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('http://gw.roigames.co.kr/')
+    await page.screenshot({'path': 'example.png'})
+    cookies = await page.cookies()
+    print(cookies)    
+    await page.evaluate('''() => {        
+        document.getElementById("gw_user_id").value = "";
+        document.getElementById("gw_user_pw").value = "";
+        encryptSubmit();        
+    }''')
+	
+    sleep(3)
+    cookies = await page.cookies()
+    print(cookies)
+	
+    page.on('dialog', lambda dialog: asyncio.ensure_future(close_dialog(dialog)))
+    await page.goto('http://gw.roigames.co.kr/chtml/groupware/groupware_popup.php?file=gw_indolence_input&mode=attendance_out&employee_id=neosdc')
+    sleep(3)
+	
+    #await page.screenshot({'path': 'example2.png'})
+    print(await page.content())
+
+    #print(dimensions)
+    # >>> {'width': 800, 'height': 600, 'deviceScaleFactor': 1}
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
+
