55291e1c777adeed37e514a6189e4fa31f428055
Kim Seung Young <neosdc@gmail.com>
2019-07-24 18:16:09 +0900

    Update elasticsearch_bulk.py

--- elasticsearch_bulk.py
+++ elasticsearch_bulk.py
@@ -1,11 +1,12 @@
-# set up - tested under es 7.2
+# set up
 from elasticsearch import Elasticsearch
 from tika import parser
 import json
-es = Elasticsearch("localhost:9200")
+from django.conf import settings
 
-es.indices.create(
-    index='bookshelf',
+es = Elasticsearch(settings.ELASTICSEARCH_HOST)
+
+es.indices.create(index='bookshelf',
     body={
         "settings": {
             "index": {
@@ -20,16 +21,18 @@ es.indices.create(
             }
         }
     }
+    , ignore=[400]
 )
-
+#delete index
+es.indices.delete(index='bookshelf', ignore=[400, 404])
 #bulk insert
-parsed = parser.from_file('test.pdf', xmlContent=True)
+parsed = parser.from_file('Swift Development with Cocoa.pdf', xmlContent=True)
 paged = parsed["content"].split('<div class="page">')
 body = ""
 iPage = 1
 for i in paged[1:-2]:
     body = body + json.dumps({"index": {"_index": "bookshelf", "_type": "bookshelf_datas"}}) + '\n'
-    body = body + json.dumps({"title":"test.pdf", "page":iPage, "content":paged[iPage]}) + '\n'
+    body = body + json.dumps({"title":"Swift Development with Cocoa.pdf", "page":iPage, "content":paged[iPage]}) + '\n'
     iPage = iPage + 1
 es.bulk(body)
 
@@ -37,3 +40,8 @@ es.bulk(body)
 es.delete_by_query(index='bookshelf', body={"query":{"match_all":{}}})
 #search docs
 docs = es.search(index='bookshelf', body={"query":{"multi_match":{"query": "Spam Filter", "fields":["content"]}}})
+#search title exact match
+titles = es.search(index='bookshelf', body={"_source":["title"],"query":{"bool":{"must":[{"match_phrase": {"title": "Swift Development.pdf"}},{"multi_match": {"query": "Swift Development.pdf","type": "most_fields","fields": ["title"] }}]}}})
+
+#mapping type update
+es.indices.put_mapping({ "properties": { "title": { "type": "text", "fielddata", true}}}, index='bookshelf') 
